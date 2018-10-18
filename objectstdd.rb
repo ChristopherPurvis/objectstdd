8366d2b4acc24a15e2f50c981293def4f71c6a7c
Chris Purvis <christopher.purvis@justice.gov.uk>
2018-10-18 11:56:41 +0100

    Circle second test
    Added the second test to the rspec file, tested failed. Amended prod
    file and ran test which passed.

--- objectstdd.rb
+++ objectstdd.rb
@@ -6,4 +6,22 @@ class Circle
     def area
        Math::PI*(@radius**2)
     end
+
+    def description
+        description = "A circle with area " + area.to_f.round(2).to_s + "cm2" + " and colour " + @colour 
+        description
+    end
+
+    #def colour
+        #@colour
+    #end
+
+
+    # enables you to input data (write it) to inside the object for use there 
+    # e.g. where we use circle.colour = "Green"
+    attr_writer :colour
+    
+    # makes data within the object public so it can be used outside the object 
+    # e.g. when you use puts to print the value outside of the object
+    attr_reader :colour
 end
--- spec/objects_spec.rb
+++ spec/objects_spec.rb
@@ -4,8 +4,13 @@ describe 'Area of a Circle' do
 
     it 'Calculates the area of a circle' do
         circle = Circle.new(5)
-
         expect(circle.area).to be_within(0.1).of(78.5)
     end
     
+    it 'Provides the output from the circle are calculation and colour' do
+        circle = Circle.new(5)
+        circle.colour = "Red"
+        expect(circle.description).to eq "A circle with area " + "78.54" + "cm2" + " and colour " + "Red"
+    end
+
 end
